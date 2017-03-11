161415e37a3fe014f70d6da0a2363a3b420ace4e
Alex <ax2@princeton.edu>
2017-03-11 18:50:31 -0500

    heroku?

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python rn.py
--- run.py
+++ run.py
@@ -1,3 +1,4 @@
-#!flask/bin/python
-from app import app
-app.run(debug=True)
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
