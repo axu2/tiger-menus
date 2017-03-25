0083c2722ee5ef146e808981b621107b7e1cfcb3
Alex Xu <ax2@princeton.edu>
2017-03-25 14:39:42 -0400

    Delete testalex.py

--- app/testalex.py
+++
@@ -1,9 +0,0 @@
-import urllib2
-from bs4 import BeautifulSoup
-from flask import render_template
-from app import app
-
-response = urllib2.urlopen('https://reg-captiva.princeton.edu/chart/index.php?terminfo=1164&courseinfo=007998')
-html = response.read()
-soup = BeautifulSoup(html, 'html.parser')
-print(soup.prettify())
