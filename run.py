62f2413f199c78aa10cbc39d010d4142f884b9cf
Alex <ax2@princeton.edu>
2017-05-05 18:37:31 -0400

    Add browser tab favicons

---
+++ run.py
@@ -0,0 +1,8 @@
+import os
+from app import app
+
+#needed for Heroku
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
