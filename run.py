37a1eca577aea3e4bbdd40d432fbc8299c8d0e30
axu2 <ax2@princeton.edu>
2017-05-17 12:30:10 -0400

    add meme

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ from app import app
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
