5506eba18ee2ddbd273112c7d50ebaa17543905e
Jeongmin Cho <jeongcho96@gmail.com>
2018-12-31 15:51:04 -0500

    Add media queries in css and JS for responsive design

--- app/static/myscript.js
+++ app/static/myscript.js
@@ -1,7 +1,25 @@
+const phone = window.matchMedia('(min-width: 320px) and (max-width: 480px)');
+const tablet = window.matchMedia('(min-width: 768px) and (max-width: 1024px)');
+const landscape = window.matchMedia('(min-width: 500px) and (max-height: 600px)');
+
+if(phone.matches) {
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+  }
+ } else if(tablet.matches) {
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "25%";
+  }
+ } else if(landscape.matches){
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "30%";
+  }
+ } else {
 function openNav() {
   document.getElementById("mySidenav").style.width = "15%";
   }
-  
+}
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-  }
+
+}
