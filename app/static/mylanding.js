1afe11c78b67511730a12f1d5af7da8846afa154
Jeongmin Cho <jeongcho96@gmail.com>
2018-12-31 16:06:20 -0500

    Fix padding on landing page quote

--- app/static/mylanding.js
+++ app/static/mylanding.js
@@ -1,11 +1,11 @@
-if(phone.matches) {
+if (phone.matches) {
   function openNav() {
     document.getElementById("mySidenav").style.width = "100%";
   }
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
- } else if(tablet.matches) {
+} else if (tablet.matches) {
   function openNav() {
     document.getElementById("mySidenav").style.width = "25%";
   }
@@ -13,28 +13,26 @@ if(phone.matches) {
     document.getElementById("mySidenav").style.width = "0";
   }
 } else if (landscape.matches) {
-    function openNav() {
-      document.getElementById("mySidenav").style.width = "30%";
-    }
-    function closeNav() {
-      document.getElementById("mySidenav").style.width = "0";
-    }
- } 
- else {
-
-function openNav() {
-  document.getElementById("mySidenav").style.width = "15%";
-  document.getElementById("bg").style.marginRight = "15%";
-  document.getElementById("bg").style.width = "85%";
-  document.getElementById("navbar").style.width = "85%";
-  document.getElementById("landing").style.padding = "25rem 21rem 26rem 21rem";
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "30%";
+  }
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
 }
+else {
 
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("bg").style.marginRight = "0";
-  document.getElementById("bg").style.width = "100%";
-  document.getElementById("navbar").style.width = "100%";
-  document.getElementById("landing").style.padding = "25rem 29rem 26rem 29rem";
-}
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "15%";
+    document.getElementById("bg").style.marginRight = "15%";
+    document.getElementById("bg").style.width = "85%";
+    document.getElementById("navbar").style.width = "85%";
+  }
+
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("bg").style.marginRight = "0";
+    document.getElementById("bg").style.width = "100%";
+    document.getElementById("navbar").style.width = "100%";
+  }
 }
