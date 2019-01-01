f5a00090834bd61f86f926e1040505e8e804df00
Jeongmin Cho <jeongcho96@gmail.com>
2019-01-01 15:56:34 -0500

    Fix font location

--- app/static/myscript.js
+++ app/static/myscript.js
@@ -1,6 +1,6 @@
-const phone = window.matchMedia('(min-width: 320px) and (max-width: 480px)');
-const tablet = window.matchMedia('(min-width: 768px) and (max-width: 1024px)');
-const landscape = window.matchMedia('(min-width: 500px) and (max-height: 600px)');
+var phone = window.matchMedia('(min-width: 320px) and (max-width: 480px)');
+var tablet = window.matchMedia('(min-width: 768px) and (max-width: 1024px)');
+var landscape = window.matchMedia('(min-width: 500px) and (max-height: 600px)');
 
 if(phone.matches) {
   function openNav() {
