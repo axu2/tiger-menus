e8739ca5360a73898745f0d64fc90aea23110c8c
Jeongmin Cho <jeongcho96@gmail.com>
2019-01-01 16:29:21 -0500

    Add Flask Compress

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_cors import CORS
 from flask_admin import Admin
 from datetime import datetime
 from flask_bootstrap import Bootstrap
+from flask.ext.compress import Compress
 from flask_mongoengine import MongoEngine
 from flask_admin.contrib.mongoengine import ModelView
 
@@ -20,6 +21,7 @@ app.config['CAS_AFTER_LOGIN'] = 'index'
 cas = CAS(app)
 CORS(app)
 Bootstrap(app)
+Compress(app)
 
 dummy = "mongodb://heroku_t6g31fxj:8u1g6iotm21eg7r11u6ai38j20@ds013495.mlab.com:13495/heroku_t6g31fxj"
 app.config['MONGODB_SETTINGS'] = {
