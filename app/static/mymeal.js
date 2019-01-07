99f23b1886b2816a2a8499aa0e19e6e5a4230bb1
Jeongmin Cho <jeongcho96@gmail.com>
2019-01-07 14:10:44 +0900

    Fix IE Edge Laptop landscape matchMedia query

--- app/static/mymeal.js
+++ app/static/mymeal.js
@@ -1,7 +1,7 @@
 var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 var phone = window.matchMedia('(min-width: 320px) and (max-width: 480px)');
 var tablet = window.matchMedia('(min-width: 768px) and (max-width: 1024px)');
-var landscape = window.matchMedia('(min-width: 500px) and (max-height: 600px)');
+var landscape = window.matchMedia('(min-width: 500px) and (max-device-width : 1024px) and (max-height: 600px) and (orientation: landscape)');
 
 var mySideNav = document.getElementById("mySidenav");
 var myBackground = document.getElementById("bg");
