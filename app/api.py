740dc5f038ca9342d74611a44eeb6b18410541ba
Alex Xu <alexkurosakimh3@gmail.com>
2021-08-21 20:07:26 -0700

    Delete api.py

--- app/api.py
+++
@@ -1,9 +0,0 @@
-from app import app
-from flask import jsonify
-from app import views
-
-
-@app.route('/api2')
-def api3():
-    """Return week's menus in JSON."""
-    return jsonify([views.breakfastLists, views.lunchLists, views.dinnerLists])
