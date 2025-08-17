ff4b7f2394d559b1589e031a5bfe5ea998baad9d
Alex Xu <alexkurosakimh3@gmail.com>
2025-08-16 21:49:05 -0700

    fix July 20 2025 changes

--- app/scrape.py
+++ app/scrape.py
@@ -34,6 +34,7 @@ def floatMainEntrees(items):
 
 
 def scrapeHall(url):
+    print(url)
     breakfast = ["No Data Available"]
     lunch = ["No Data Available"]
     dinner = ["No Data Available"]
@@ -46,8 +47,15 @@ def scrapeHall(url):
         html = requests.get(url).text
         soup = BeautifulSoup(html, 'html.parser')
         for card in soup.findAll(class_="card"):
-            mealName = card.h5.text
-            items = floatMainEntrees(list(card.ul.stripped_strings))
+            mealName = card.find(class_='card-header').contents[0].strip()
+            items = card.find(class_='card-body').findAll(class_=["mealStation", "title"])
+            formatted_items = []
+            for item in items:
+                if 'mealStation' in item['class']:
+                    formatted_items.append(f"-- {item.text.strip()} --")
+                else:
+                    formatted_items.append(item.text.strip())
+            items = floatMainEntrees(formatted_items)
             if mealName == b:
                 breakfast = [b] + items
             elif mealName == l:
@@ -77,7 +85,7 @@ def scrapeDay(m, d, y):
     dayLunch = []
     dayDinner = []
 
-    prefix = "https://menus.princeton.edu/dining/_Foodpro/online-menu/menuDetails.asp?"
+    prefix = "https://menus.princeton.edu/dining/_Foodpro/online-menu/menuDetails.asp?myaction=read&"
     prefix += "dtdate={}%2F{}%2F{}&locationNum=".format(m, d, y)
 
     halls = [prefix + i for i in hallToId.values()]
